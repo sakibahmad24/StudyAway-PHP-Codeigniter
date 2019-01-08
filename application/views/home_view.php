1c27b37f80b5fc4601beb01ae520305bdb543824
sakibahmad24 <sakibahmad24@gmail.com>
2019-01-08 23:38:55 +0600

    homepage frontend done

---
+++ application/views/home_view.php
@@ -0,0 +1,295 @@
+<main>
+		<section class="header-video">
+			<div id="hero_video">
+				<div>
+					<h3><strong>Top Degrees</strong><br>on Top Universities</h3>
+					<p>Find the desired degree at your convenient university to pursue <strong>Higher Studies</strong> to make your dream true</p>
+				</div>
+				<a href="#first_section" class="btn_explore hidden_tablet"><i class="ti-arrow-down"></i></a>
+			</div>
+			<img src="img/video_fix.png" alt="" class="header-video--media" data-video-src="video/intro" data-teaser-source="video/intro" data-provider="" data-video-width="1920" data-video-height="960">
+		</section>
+		<!-- /header-video -->
+
+		<ul id="grid_home" class="clearfix">
+			<li>
+				<a href="#0" class="img_container">
+					<img src="http://via.placeholder.com/600x400/ccc/fff/grid_home_1.jpg" alt="">
+					<div class="short_info">
+						<h3><strong>Latest Updates</strong> on master's degrees</h3>
+						<div><span class="btn_1 rounded">Read more</span></div>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a href="#0" class="img_container">
+					<img src="http://via.placeholder.com/600x400/ccc/fff/grid_home_1.jpg" alt="">
+					<div class="short_info">
+						<h3><strong>University</strong>all over the globe</h3>
+						<div><span class="btn_1 rounded">Read more</span></div>
+					</div>
+				</a>
+			</li>
+			<li>
+				<a href="#0" class="img_container">
+					<img src="http://via.placeholder.com/600x400/ccc/fff/grid_home_1.jpg" alt="">
+					<div class="short_info">
+						<h3><strong>Admission</strong>Apply Online</h3>
+						<div><span class="btn_1 rounded">Submit</span></div>
+					</div>
+				</a>
+			</li>
+		</ul>
+		<!--/grid_home -->
+
+		<div class="container-fluid margin_120_0" id="first_section">
+			<div class="main_title_2">
+				<span><em></em></span>
+				<h2>Study Away popular Guides</h2>
+				<p>Most popular student guides to help out for making right decisions</p>
+			</div>
+			<div id="reccomended" class="owl-carousel owl-theme">
+				<div class="item">
+					<div class="box_grid">
+						<figure>
+							<a href="#0" class="wish_bt"></a>
+							<a href="course-detail.html">
+								<div class="preview"><span>Preview guide</span></div><img src="http://via.placeholder.com/800x533/ccc/fff/course__list_1.jpg" class="img-fluid" alt=""></a>
+							
+
+						</figure>
+						<div class="wrapper">
+							<small>Program</small>
+							<h3>Deep knowledge about MS in CSE</h3>
+							<p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu.</p>
+							<div class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></div>
+						</div>
+						<ul>
+							<li><i class="icon_clock_alt"></i> 1d 3hrs</li>
+							<li><i class="icon_like"></i> 890</li>
+							<li><a href="course-detail.html">View Post</a></li>
+						</ul>
+					</div>
+				</div>
+				<!-- /item -->
+				<div class="item">
+					<div class="box_grid">
+						<figure>
+							<a href="#0" class="wish_bt"></a>
+							<a href="course-detail.html">
+								<div class="preview"><span>Preview guide</span></div><img src="http://via.placeholder.com/800x533/ccc/fff/course__list_1.jpg" class="img-fluid" alt=""></a>
+							
+
+						</figure>
+						<div class="wrapper">
+							<small>Exam</small>
+							<h3>IELTS 2018</h3>
+							<p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu.</p>
+							<div class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></div>
+						</div>
+						<ul>
+							<li><i class="icon_clock_alt"></i> 1d 3hrs</li>
+							<li><i class="icon_like"></i> 890</li>
+							<li><a href="course-detail.html">View Post</a></li>
+						</ul>
+					</div>
+				</div>
+				<!-- /item -->
+				
+				<!-- /item -->
+				
+				</div>
+				<!-- /item -->
+				
+				<!-- /item -->
+				
+				<!-- /item -->
+			</div>
+			<!-- /carousel -->
+			<div class="container">
+				<p class="btn_home_align"><a href="courses-grid.html" class="btn_1 rounded">View all guides</a></p>
+			</div>
+			<!-- /container -->
+			<hr>
+		</div>
+		<!-- /container -->
+
+		<div class="container margin_30_95">
+			<div class="main_title_2">
+				<span><em></em></span>
+				<h2>Study Away Categorized Programs</h2>
+				<p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-md-6 wow" data-wow-offset="150">
+					<a href="#0" class="grid_item">
+						<figure class="block-reveal">
+							<div class="block-horizzontal"></div>
+							<img src="http://via.placeholder.com/800x533/ccc/fff/course_1.jpg" class="img-fluid" alt="">
+							<div class="info">
+								<small><i class="ti-layers"></i>15 Universities</small>
+								<h3>Computer Science</h3>
+							</div>
+						</figure>
+					</a>
+				</div>
+				<!-- /grid_item -->
+				<div class="col-lg-4 col-md-6 wow" data-wow-offset="150">
+					<a href="#0" class="grid_item">
+						<figure class="block-reveal">
+							<div class="block-horizzontal"></div>
+							<img src="http://via.placeholder.com/800x533/ccc/fff/course_2.jpg" class="img-fluid" alt="">
+							<div class="info">
+								<small><i class="ti-layers"></i>23 Universities</small>
+								<h3>Engineering</h3>
+							</div>
+						</figure>
+					</a>
+				</div>
+				<!-- /grid_item -->
+				<div class="col-lg-4 col-md-6 wow" data-wow-offset="150">
+					<a href="#0" class="grid_item">
+						<figure class="block-reveal">
+							<div class="block-horizzontal"></div>
+							<img src="http://via.placeholder.com/800x533/ccc/fff/course_3.jpg" class="img-fluid" alt="">
+							<div class="info">
+								<small><i class="ti-layers"></i>23 Universities</small>
+								<h3>Architecture</h3>
+							</div>
+						</figure>
+					</a>
+				</div>
+				<!-- /grid_item -->
+				<div class="col-lg-4 col-md-6 wow" data-wow-offset="150">
+					<a href="#0" class="grid_item">
+						<figure class="block-reveal">
+							<div class="block-horizzontal"></div>
+							<img src="http://via.placeholder.com/800x533/ccc/fff/course_4.jpg" class="img-fluid" alt="">
+							<div class="info">
+								<small><i class="ti-layers"></i>23 Universities</small>
+								<h3>Electrical Engineering</h3>
+							</div>
+						</figure>
+					</a>
+				</div>
+				<!-- /grid_item -->
+				<div class="col-lg-4 col-md-6 wow" data-wow-offset="150">
+					<a href="#0" class="grid_item">
+						<figure class="block-reveal">
+							<div class="block-horizzontal"></div>
+							<img src="http://via.placeholder.com/800x533/ccc/fff/course_5.jpg" class="img-fluid" alt="">
+							<div class="info">
+								<small><i class="ti-layers"></i>23 Universities</small>
+								<h3>Law and Criminology</h3>
+							</div>
+						</figure>
+					</a>
+				</div>
+				<!-- /grid_item -->
+				<div class="col-lg-4 col-md-6 wow" data-wow-offset="150">
+					<a href="#0" class="grid_item">
+						<figure class="block-reveal">
+							<div class="block-horizzontal"></div>
+							<img src="http://via.placeholder.com/800x533/ccc/fff/course_6.jpg" class="img-fluid" alt="">
+							<div class="info">
+								<small><i class="ti-layers"></i>23 Programmes</small>
+								<h3>Biomedical Sciences</h3>
+							</div>
+						</figure>
+					</a>
+				</div>
+				<!-- /grid_item -->
+			</div>
+			<!-- /row -->
+		</div>
+		<!-- /container -->
+
+		<div class="bg_color_1">
+			<div class="container margin_120_95">
+				<div class="main_title_2">
+					<span><em></em></span>
+					<h2>News and Events</h2>
+					<p>Cum doctus civibus efficiantur in imperdiet deterruisset.</p>
+				</div>
+				<div class="row">
+					<div class="col-lg-6">
+						<a class="box_news" href="#0">
+							<figure><img src="http://via.placeholder.com/500x333/ccc/fff/news_home_1.jpg" alt="">
+								<figcaption><strong>28</strong>Dec</figcaption>
+							</figure>
+							<ul>
+								<li>Tanjila Farah</li>
+								<li>20.11.2017</li>
+							</ul>
+							<h4>Workshop on how to apply for fund</h4>
+							<p>Cu eum alia elit, usu in eius appareat, deleniti sapientem honestatis eos ex. In ius esse ullum vidisse....</p>
+						</a>
+					</div>
+					<!-- /box_news -->
+					<div class="col-lg-6">
+						<a class="box_news" href="#0">
+							<figure><img src="http://via.placeholder.com/500x333/ccc/fff/news_home_2.jpg" alt="">
+								<figcaption><strong>28</strong>Dec</figcaption>
+							</figure>
+							<ul>
+								<li>Kazi Sakib Ahmad</li>
+								<li>20.11.2017</li>
+							</ul>
+							<h4>VISA Assistance seminar for USA</h4>
+							<p>Cu eum alia elit, usu in eius appareat, deleniti sapientem honestatis eos ex. In ius esse ullum vidisse....</p>
+						</a>
+					</div>
+					<!-- /box_news -->
+					<div class="col-lg-6">
+						<a class="box_news" href="#0">
+							<figure><img src="http://via.placeholder.com/500x333/ccc/fff/news_home_3.jpg" alt="">
+								<figcaption><strong>28</strong>Dec</figcaption>
+							</figure>
+							<ul>
+								<li>Syed Sadman</li>
+								<li>20.11.2017</li>
+							</ul>
+							<h4>Seminar on qualifying factors for higher study abroad</h4>
+							<p>Cu eum alia elit, usu in eius appareat, deleniti sapientem honestatis eos ex. In ius esse ullum vidisse....</p>
+						</a>
+					</div>
+					<!-- /box_news -->
+					<div class="col-lg-6">
+						<a class="box_news" href="#0">
+							<figure><img src="http://via.placeholder.com/500x333/ccc/fff/news_home_4.jpg" alt="">
+								<figcaption><strong>28</strong>Dec</figcaption>
+							</figure>
+							<ul>
+								<li>Sadik Khan</li>
+								<li>20.11.2017</li>
+							</ul>
+							<h4>Workshop on recommendation letter writting</h4>
+							<p>Cu eum alia elit, usu in eius appareat, deleniti sapientem honestatis eos ex. In ius esse ullum vidisse....</p>
+						</a>
+					</div>
+					<!-- /box_news -->
+				</div>
+				<!-- /row -->
+				<p class="btn_home_align"><a href="blog.html" class="btn_1 rounded">View all news</a></p>
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /bg_color_1 -->
+
+		<div class="call_section">
+			<div class="container clearfix">
+				<div class="col-lg-5 col-md-6 float-right wow" data-wow-offset="250">
+					<div class="block-reveal">
+						<div class="block-vertical"></div>
+						<div class="box_1">
+							<h3>Enjoy a great assistance on choosing higher study abroad</h3>
+							<p>Ius cu tamquam persequeris, eu veniam apeirian platonem qui, id aliquip voluptatibus pri. Ei mea primis ornatus disputationi. Menandri erroribus cu per, duo solet congue ut. </p>
+							<a href="#0" class="btn_1 rounded">Read more</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/call_section-->
+	</main>
+	<!-- /main -->
