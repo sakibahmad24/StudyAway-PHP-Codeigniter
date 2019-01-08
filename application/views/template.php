54f86a6deb52d1bb4ff0e86dace4ee84116231ea
sakibahmad24 <sakibahmad24@gmail.com>
2019-01-08 10:57:23 +0600

    template building

--- application/views/template.php
+++ application/views/template.php
@@ -10,15 +10,16 @@
     <title>Study Away | SMART HIGHER STUDY ASSISTANT</title>
 
     <!-- Favicons-->
-    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
-    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
+    <link rel="shortcut icon" href=<?php echo base_url("img/favicon.ico")?> type="image/x-icon">
+    <link rel="apple-touch-icon" type="image/x-icon" href=<?php echo base_url("img/apple-touch-icon-57x57-precomposed.png") ?> >
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href=<?php echo base_url("img/apple-touch-icon-72x72-precomposed.png")?> >
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href=<?php echo base_url("img/apple-touch-icon-114x114-precomposed.png")?> >
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href=<?php echo base_url("img/apple-touch-icon-144x144-precomposed.png")?> >
 
     <!-- BASE CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+
+    <link href=<?php echo base_url("css/bootstrap.min.css")?> rel="stylesheet">
+    <link href="css/style.css" rel=" stylesheet">
     <link href="css/vendors.css" rel="stylesheet">
     <link href="css/icon_fonts/css/all_icons.min.css" rel="stylesheet">
 
@@ -29,3 +30,7 @@
     <script src="js/modernizr.js"></script>
 
 </head>
+
+<body>
+    
+    <div id="page">
