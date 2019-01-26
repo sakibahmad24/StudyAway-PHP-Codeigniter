082d436bda88c9425842b78ae26a4e3c2160bc16
sakibahmad24 <sakibahmad24@gmail.com>
2019-01-26 13:08:42 +0600

    databse updated

--- study.sql
+++ study.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 24, 2019 at 04:53 PM
+-- Generation Time: Jan 26, 2019 at 08:06 AM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 5.6.38
 
@@ -21,6 +21,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `study`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `programs`
+--
+
+CREATE TABLE `programs` (
+  `pid` int(11) NOT NULL,
+  `region` varchar(255) NOT NULL,
+  `place` varchar(255) NOT NULL,
+  `university` varchar(255) NOT NULL,
+  `p_name` varchar(255) NOT NULL,
+  `first_year_fee` varchar(25) NOT NULL,
+  `eligibility` varchar(255) NOT NULL,
+  `uni_type` int(3) NOT NULL,
+  `scholarship` varchar(10) NOT NULL,
+  `accomodation` varchar(10) NOT NULL,
+  `link` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -46,6 +66,12 @@ INSERT INTO `users` (`sl`, `firstname`, `lastname`, `email`, `password`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `programs`
+--
+ALTER TABLE `programs`
+  ADD PRIMARY KEY (`pid`);
 
 --
 -- Indexes for table `users`
